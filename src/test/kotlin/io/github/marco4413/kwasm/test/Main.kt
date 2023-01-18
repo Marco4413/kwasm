5cc166df0a399ddf6f064b87c7601322e6b2ee50
Marco <46133335+Marco4413@users.noreply.github.com>
2023-01-18 15:54:37 +0100

    Added Test Kotlin Module

---
+++ src/test/kotlin/io/github/marco4413/kwasm/test/Main.kt
@@ -0,0 +1,13 @@
+package io.github.marco4413.kwasm.test
+
+import io.github.marco4413.kwasm.Module
+
+fun main(args: Array<String>) {
+    if (args.isEmpty())
+        throw NullPointerException("Resource to open not specified.")
+    val resourceName = args[0]
+    val module = Module.fromStream(
+        Module.Companion::class.java.getResourceAsStream(resourceName)
+            ?: throw NullPointerException("Couldn't open resource '$resourceName'")
+    )
+}
