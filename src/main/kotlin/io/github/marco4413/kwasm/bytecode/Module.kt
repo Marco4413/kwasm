050cdb962a9d36ce8de017e45716c6595ac4575d
Marco <46133335+Marco4413@users.noreply.github.com>
2023-01-21 19:27:54 +0100

    Module magic and version is now checked

--- src/main/kotlin/io/github/marco4413/kwasm/bytecode/Module.kt
+++ src/main/kotlin/io/github/marco4413/kwasm/bytecode/Module.kt
@@ -1,21 +1,34 @@
 package io.github.marco4413.kwasm.bytecode
 
 import io.github.marco4413.kwasm.bytecode.section.*
+import io.github.marco4413.kwasm.bytecode.section.Function
 import java.io.InputStream
+import java.lang.RuntimeException
 
-class Module(val magic: U32, val version: U32,
+class InvalidWasmFile :
+    RuntimeException("Invalid Wasm file.")
+class UnsupportedWasmVersion(got: U32, expected: U32) :
+    RuntimeException("Unsupported Wasm version $got, supported $expected.")
+
+data class Module(val magic: U32, val version: U32,
              val types: TypeSection, val functions: FunctionSection,
              val exports: ExportSection, val code: CodeSection) {
     companion object {
+        const val WASM_MAGIC: U32 = 1836278016u
+        const val WASM_VERSION: U32 = 1u
+
         fun fromStream(inStream: InputStream) : Module {
             val s = WasmInputStream(inStream)
 
             val magic = s.readRawU32()
+            if (magic != WASM_MAGIC) throw InvalidWasmFile()
             val version = s.readRawU32()
-            val types = TypeSection()
-            val functions = FunctionSection()
-            val exports = ExportSection()
-            val code = CodeSection()
+            if (version != WASM_VERSION) throw UnsupportedWasmVersion(version, WASM_VERSION)
+
+            val types = ArrayList<FunctionType>()
+            val functions = ArrayList<TypeIdx>()
+            val exports = ArrayList<Export>()
+            val code = ArrayList<Function>()
 
             while (s.available() > 0) {
                 when (val sId = s.readU8()) {
