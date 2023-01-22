6baef51594fd39f3a88536ed4eaac05c75782ce0
Marco <46133335+Marco4413@users.noreply.github.com>
2023-01-22 14:52:16 +0100

    Local Instructions now use LocalIdx

--- src/main/kotlin/io/github/marco4413/kwasm/instructions/Local.kt
+++ src/main/kotlin/io/github/marco4413/kwasm/instructions/Local.kt
@@ -1,16 +1,16 @@
 package io.github.marco4413.kwasm.instructions
 
+import io.github.marco4413.kwasm.bytecode.LocalIdx
 import io.github.marco4413.kwasm.bytecode.U32
 import io.github.marco4413.kwasm.bytecode.WasmInputStream
 import io.github.marco4413.kwasm.runtime.Configuration
 import io.github.marco4413.kwasm.runtime.Stack
-import io.github.marco4413.kwasm.runtime.ValueI32
 
 val LocalGetDescriptor = object : InstructionDescriptor("local.get", 0x20u) {
     override fun read(s: WasmInputStream): Instruction = LocalGet(s.readU32())
 }
 
-class LocalGet(val index: U32) : Instruction(LocalGetDescriptor) {
+class LocalGet(val index: LocalIdx) : Instruction(LocalGetDescriptor) {
     override fun execute(config: Configuration, stack: Stack) {
         val local = config.thread.frame.locals[index.toInt()]
         stack.pushValue(local)
@@ -22,7 +22,7 @@ val LocalSetDescriptor = object : InstructionDescriptor("local.set", 0x21u) {
     override fun read(s: WasmInputStream): Instruction = LocalSet(s.readU32())
 }
 
-class LocalSet(val index: U32) : Instruction(LocalSetDescriptor) {
+class LocalSet(val index: LocalIdx) : Instruction(LocalSetDescriptor) {
     override fun execute(config: Configuration, stack: Stack) {
         val value = stack.popValue()
         config.thread.frame.locals[index.toInt()] = value
@@ -34,7 +34,7 @@ val LocalTeeDescriptor = object : InstructionDescriptor("local.tee", 0x22u) {
     override fun read(s: WasmInputStream): Instruction = LocalTee(s.readU32())
 }
 
-class LocalTee(val index: U32) : Instruction(LocalTeeDescriptor) {
+class LocalTee(val index: LocalIdx) : Instruction(LocalTeeDescriptor) {
     override fun execute(config: Configuration, stack: Stack) {
         val value = stack.popValue()
         stack.pushValue(value)
