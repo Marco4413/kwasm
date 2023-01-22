76a871c2c4900ef10333bea429963c069ce374e9
Marco <46133335+Marco4413@users.noreply.github.com>
2023-01-22 23:12:12 +0100

    Added FunctionType#signatureEquals

--- src/main/kotlin/io/github/marco4413/kwasm/bytecode/section/TypeSection.kt
+++ src/main/kotlin/io/github/marco4413/kwasm/bytecode/section/TypeSection.kt
@@ -7,7 +7,24 @@ import io.github.marco4413.kwasm.bytecode.WasmInputStream
 
 const val FunctionTypeId: UByte = 96u
 class FunctionType(val parameters: List<ValueType>,
-                        val results: List<ValueType>)
+                   val results: List<ValueType>) {
+    fun signatureEquals(other: FunctionType) : Boolean {
+        if (parameters.size != other.parameters.size ||
+            results.size != other.results.size) return false
+
+        for (i in parameters.indices) {
+            if (parameters[i] != other.parameters[i])
+                return false
+        }
+
+        for (i in results.indices) {
+            if (results[i] != other.results[i])
+                return false
+        }
+
+        return true
+    }
+}
 
 const val TypeSectionId: U8 = 1u
 typealias TypeSection = List<FunctionType>
