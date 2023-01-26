e502b4307ab26480d6f15a049370062608d69828
Marco <46133335+Marco4413@users.noreply.github.com>
2023-01-26 20:44:54 +0100

    Added Memory#grow

--- src/main/kotlin/io/github/marco4413/kwasm/runtime/Memory.kt
+++ src/main/kotlin/io/github/marco4413/kwasm/runtime/Memory.kt
@@ -1,24 +1,28 @@
 package io.github.marco4413.kwasm.runtime
 
-import io.github.marco4413.kwasm.bytecode.I32
-import io.github.marco4413.kwasm.bytecode.U32
-import io.github.marco4413.kwasm.bytecode.U8
+import io.github.marco4413.kwasm.bytecode.*
 import io.github.marco4413.kwasm.bytecode.section.MemoryType
 
-class MemoryInstance(val type: MemoryType) {
+class MemoryInstance(type: MemoryType) {
     companion object {
-        const val PAGE_SIZE: U32 = 65532u
+        const val PAGE_SIZE: U32 = 65536u // 2^16
     }
 
-    var size: U32 private set
-    var data: MutableList<U8> private set
+    var type = type
+        private set
+    val pages: U32 get() = type.min
+    var data: MutableList<U8> = ArrayList(List((pages * PAGE_SIZE).toInt()) { 0u })
+        private set
 
-    init {
-        size = type.limit.min.coerceAtLeast(1u)
-        data = ArrayList(List((size * PAGE_SIZE).toInt()) { 0u })
+    fun grow(n: U32) {
+        assert(data.size.toUInt() % PAGE_SIZE == 0u)
+        val len = pages + n
+        if (len > PAGE_SIZE) TODO("B I G Memory")
+        val limits1 = MemoryType(len, type.max)
+        if (!limits1.isValid()) TODO("Invalid Limits")
+        for (i in 0u until (n * PAGE_SIZE)) data.add(0u)
+        type = limits1
     }
-
-    // TODO: Grow
 
     operator fun get(index: U32) : U8 = this[index.toInt()]
     operator fun get(index: I32) : U8 {
