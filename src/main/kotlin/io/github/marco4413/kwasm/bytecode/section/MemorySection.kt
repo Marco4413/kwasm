e8f55d6022d62036dd6487385aadbeaeeab344ed
Marco <46133335+Marco4413@users.noreply.github.com>
2023-01-26 20:43:32 +0100

    Reading Limits correctly

--- src/main/kotlin/io/github/marco4413/kwasm/bytecode/section/MemorySection.kt
+++ src/main/kotlin/io/github/marco4413/kwasm/bytecode/section/MemorySection.kt
@@ -6,10 +6,19 @@ import io.github.marco4413.kwasm.bytecode.WasmInputStream
 
 class Limit(val min: U32, val max: U32 = U32.MAX_VALUE) {
     private val _range = min..max
+    fun isValid() : Boolean = min <= max
     operator fun contains(v: U32) : Boolean = v in _range
 }
 
-class MemoryType(val limit: Limit)
+fun readLimit(s: WasmInputStream) : Limit {
+    return when (s.readU8().toUInt()) {
+        0u -> Limit(s.readU32())
+        1u -> Limit(s.readU32(), s.readU32())
+        else -> throw IllegalStateException()
+    }
+}
+
+typealias MemoryType = Limit
 class Memory(val type: MemoryType)
 
 const val MemorySectionId: U8 = 5u
@@ -17,5 +26,5 @@ typealias MemorySection = List<Memory>
 
 fun readMemorySection(s: WasmInputStream) : MemorySection {
     s.readU32() // SIZE
-    return s.readVector { Memory(MemoryType(Limit(s.readU32(), s.readU32()))) }
+    return s.readVector { Memory(readLimit(s)) }
 }
