33989b62f86d4f778c7a146e7aabe3542d0b29f3
Marco <46133335+Marco4413@users.noreply.github.com>
2023-01-30 18:11:24 +0100

    Added i32.lt_u|le_u|mul

--- src/main/kotlin/io/github/marco4413/kwasm/instructions/I32.kt
+++ src/main/kotlin/io/github/marco4413/kwasm/instructions/I32.kt
@@ -30,6 +30,20 @@ class I32EqZ : Instruction(I32EqZDescriptor) {
     }
 }
 
+val I32LTUDescriptor = object : InstructionDescriptor("i32.lt_u", 0x49u) {
+    override fun read(s: WasmInputStream): Instruction = I32LTU()
+}
+
+class I32LTU : Instruction(I32LTUDescriptor) {
+    override fun execute(config: Configuration, stack: Stack) {
+        val b = stack.popValue() as ValueI32
+        val a = stack.popValue() as ValueI32
+        stack.pushValue(ValueI32(
+            if (a.value.toUInt() < b.value.toUInt()) 1 else 0
+        ))
+    }
+}
+
 val I32GTUDescriptor = object : InstructionDescriptor("i32.gt_u", 0x4Bu) {
     override fun read(s: WasmInputStream): Instruction = I32GTU()
 }
@@ -40,6 +54,20 @@ class I32GTU : Instruction(I32GTUDescriptor) {
         val a = stack.popValue() as ValueI32
         stack.pushValue(ValueI32(
             if (a.value.toUInt() > b.value.toUInt()) 1 else 0
+        ))
+    }
+}
+
+val I32LEUDescriptor = object : InstructionDescriptor("i32.le_u", 0x4Du) {
+    override fun read(s: WasmInputStream): Instruction = I32LEU()
+}
+
+class I32LEU : Instruction(I32LEUDescriptor) {
+    override fun execute(config: Configuration, stack: Stack) {
+        val b = stack.popValue() as ValueI32
+        val a = stack.popValue() as ValueI32
+        stack.pushValue(ValueI32(
+            if (a.value.toUInt() <= b.value.toUInt()) 1 else 0
         ))
     }
 }
@@ -65,6 +93,18 @@ class I32Sub : Instruction(I32SubDescriptor) {
         val b = stack.popValue() as ValueI32
         val a = stack.popValue() as ValueI32
         stack.pushValue(ValueI32(a.value - b.value))
+    }
+}
+
+val I32MulDescriptor = object : InstructionDescriptor("i32.mul", 0x6Cu) {
+    override fun read(s: WasmInputStream): Instruction = I32Mul()
+}
+
+class I32Mul : Instruction(I32MulDescriptor) {
+    override fun execute(config: Configuration, stack: Stack) {
+        val b = stack.popValue() as ValueI32
+        val a = stack.popValue() as ValueI32
+        stack.pushValue(ValueI32(a.value * b.value))
     }
 }
 
